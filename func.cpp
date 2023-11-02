d21d86e4a07a385a7694652f671d212132668e4d
MaksimMyson <maksimmyson33@gmail.com>
2023-11-02 11:22:58 +0200

    ex1

--- func.cpp
+++ func.cpp
@@ -1,2 +1,11 @@
 #include "func.h"
-using namespace std;
+#include <iostream>
+
+void printRectangle(int height, int width) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "func.h"
-using namespace std;
 
-int main()
-{
-	return 0;
+int main() {
+    int height, width;
+
+    std::cout << "Enter the height of the rectangle: ";
+    std::cin >> height;
+
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    printRectangle(height, width);
+
+    return 0;
 }

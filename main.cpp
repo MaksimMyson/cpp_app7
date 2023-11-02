bab08445939f7407ae17a383471f841805789df4
MaksimMyson <maksimmyson33@gmail.com>
2023-11-02 11:33:49 +0200

    ex3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include "func.h"
-using namespace std;
 
-int main()
-{
-	return 0;
+int main() {
+    int num;
+    std::cout << "Enter a number to check for primality: ";
+    std::cin >> num;
+
+    if (isPrime(num)) {
+        std::cout << num << " is a prime number." << std::endl;
+    }
+    else {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    return 0;
 }

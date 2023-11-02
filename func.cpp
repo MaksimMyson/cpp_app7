841f796b42a2c1cb5fcc31e998c078968841c10f
MaksimMyson <maksimmyson33@gmail.com>
2023-11-02 11:29:10 +0200

    ex2

--- func.cpp
+++ func.cpp
@@ -1,2 +1,14 @@
 #include "func.h"
-using namespace std;
+
+
+int factorial(int n) {
+    if (n < 0) {
+        return -1; 
+    }
+
+    int result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
+#include <iostream>
 #include "func.h"
-using namespace std;
 
-int main()
-{
-	return 0;
+int main() {
+    int n;
+    std::cout << "Enter a number to compute its factorial: ";
+    std::cin >> n;
+
+    int result = factorial(n);
+
+    if (result == -1) {
+        std::cout << "Factorial is defined only for non-negative numbers." << std::endl;
+    }
+    else {
+        std::cout << "Factorial of " << n << " = " << result << std::endl;
+    }
+
+    return 0;
 }
